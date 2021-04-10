6d11a9dcfd07b0ee1a8180e8a5ca445c12b3b27e
thomaslwk <leethomaswg@gmail.com>
2021-04-10 07:32:51 -0700

    update lect 2 example

--- java_lect/lect2/Shapes.java
+++ java_lect/lect2/Shapes.java
@@ -1,18 +1,25 @@
-
+import java.awt.*;
 public class Shapes {
-
 	private int length,breadth, radius;
 	public Shapes(int r) {
 		radius = r;
+		System.out.println(3*(r*r)); //classic 
 	}
 	public Shapes(int l,int b) {
 		length = l;
 		breadth = b;
+		System.out.println(l*b);
+	}
+	// For square, where s for side
+	public Shapes(double s){
+		length = (int) s;
+		breadth = (int) s;
+		System.out.println(s*s);
 	}
 	public static void main(String args[]) {
 		Shapes circle = new Shapes(6);
 		Shapes rectctangle = new Shapes(10,5);
-		
 		//what if you want to initialize a square?
+		Shapes square = new Shapes(8.0);
 	}
 }
--- java_lect/lect2/TestTime.java
+++ java_lect/lect2/TestTime.java
@@ -1,19 +1,29 @@
 class Time{
-	int hour = 1;
-	int minute = 1;
-	int sec = 1;
-	
+	// int hour = 1;
+	// int minute = 1;
+	// int sec = 1;
+
+	private int hour = 1;
+	private int minute = 2;
+	private int sec = 3;
+	public int getHour(){return hour;}
+	public int getMinute(){return minute;}
+	public int getSec(){return sec;}
 	// what is we change the access modifier to private or protected or public?
 	public void display() {
-		System.out.println("Current time is "+hour+":"+minute+":"+sec);
+		// System.out.println("Current time is "+hour+":"+minute+":"+sec);
+		System.out.println("Current time is "+getHour()+":"+getMinute()+":"+getSec());
 	}
 }
 public class TestTime {
 	public static void main(String args[]) {
 		Time newTime = new Time();
-		System.out.println(newTime.hour);
-		System.out.println(newTime.minute);		
-		System.out.println(newTime.sec);
+		// System.out.println(newTime.hour);
+		// System.out.println(newTime.minute);		
+		// System.out.println(newTime.sec);
+		System.out.println(newTime.getHour());
+		System.out.println(newTime.getMinute());		
+		System.out.println(newTime.getSec());
 		newTime.display();
 	}
 }
